4e5a70899b68f6802e2fb374f0083d750ba475b5
Joanne <limruoqijoanne54@gmail.com>
2016-07-01 19:43:04 +0800

    bj

--- Web App Project/Web App Project/ASPX Files/Joanne/updateParticulars.aspx.cs
+++ Web App Project/Web App Project/ASPX Files/Joanne/updateParticulars.aspx.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
 
 namespace Web_App_Project.ASPX_Files.Joanne
 {
@@ -16,6 +19,28 @@ namespace Web_App_Project.ASPX_Files.Joanne
 
         protected void Button1_Click(object sender, EventArgs e)
         {
+            using (SqlConnection myConnection = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["localdbConnectionString1"].ConnectionString))
+            {
+                string fName = TextBox1.Text;
+                string lName = TextBox2.Text;
+                string email = TextBox3.Text;
+                string addr = TextBox4.Text;
+
+
+                string query = "UPDATE Accounts (fName, lName, email, telNo, addr)";
+                query += "VALUES (@FName, @LName, @Gender, @NRIC, @Birthdate, @Email, @TelNo, @Password, @Occupation, @Address, @Salutation)";
+
+                SqlCommand myCommand = new SqlCommand(query, myConnection);
+
+                myCommand.Parameters.AddWithValue("@FName", fName);
+                myCommand.Parameters.AddWithValue("@LName", lName);
+                myCommand.Parameters.AddWithValue("@Email", email);
+                myCommand.Parameters.AddWithValue("@Address", addr);
+                
+                myConnection.Open();
+                myCommand.ExecuteNonQuery();
+                myConnection.Close();
+            }
             Response.Redirect("updatedText.aspx");
         }
     }
