462f109be381e5bab0071535af1d5b7d7fe5018d
Angir <isadlife@gmail.com>
2016-07-04 21:31:04 +0800

    Added PayPal page (To be Edited)

---
+++ Web App Project/Web App Project/ASPX Files/Seri/Paypal.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Web_App_Project.ASPX_Files.Seri
+{
+    public partial class Paypal : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
