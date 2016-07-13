95b02359a3c312ef6f614c80b1862b723d22ca1a
Ryan <ryanchew96@gmail.com>
2016-07-13 16:39:19 +0800

    Small update

--- Web App Project/Web App Project/ASPX Files/Joanne/Hash.cs
+++ Web App Project/Web App Project/ASPX Files/Joanne/Hash.cs
@@ -21,29 +21,6 @@ namespace Web_App_Project.ASPX_Files.Joanne
         /// also opt to keep results as byte arrays instead of converting them into
         /// base64-encoded strings.
 
-        /// <summary>
-        /// Generates a hash for the given plain text value and returns a
-        /// base64-encoded result. Before the hash is computed, a random salt
-        /// is generated and appended to the plain text. This salt is stored at
-        /// the end of the hash value, so it can be used later for hash
-        /// verification.
-        /// </summary>
-        /// <param name="plainText">
-        /// Plaintext value to be hashed. The function does not check whether
-        /// this parameter is null.
-        /// </param>
-        /// <param name="hashAlgorithm">
-        /// Name of the hash algorithm. Allowed values are: "MD5", "SHA1",
-        /// "SHA256", "SHA384", and "SHA512" (if any other value is specified
-        /// MD5 hashing algorithm will be used). This value is case-insensitive.
-        /// </param>
-        /// <param name="saltBytes">
-        /// Salt bytes. This parameter can be null, in which case a random salt
-        /// value will be generated.
-        /// </param>
-        /// <returns>
-        /// Hash value formatted as a base64-encoded string.
-        /// </returns>
         public static string ComputeHash(string plainText,
                                          string hashAlgorithm,
                                          byte[] saltBytes)
@@ -139,28 +116,9 @@ namespace Web_App_Project.ASPX_Files.Joanne
             return hashValue;
         }
 
-        /// <summary>
         /// Compares a hash of the specified plain text value to a given hash
         /// value. Plain text is hashed with the same salt value as the original
         /// hash.
-        /// </summary>
-        /// <param name="plainText">
-        /// Plain text to be verified against the specified hash. The function
-        /// does not check whether this parameter is null.
-        /// </param>
-        /// <param name="hashAlgorithm">
-        /// Name of the hash algorithm. Allowed values are: "MD5", "SHA1", 
-        /// "SHA256", "SHA384", and "SHA512" (if any other value is specified,
-        /// MD5 hashing algorithm will be used). This value is case-insensitive.
-        /// </param>
-        /// <param name="hashValue">
-        /// Base64-encoded hash value produced by ComputeHash function. This value
-        /// includes the original salt appended to it.
-        /// </param>
-        /// <returns>
-        /// If computed hash mathes the specified hash the function the return
-        /// value is true; otherwise, the function returns false.
-        /// </returns>
         public static bool VerifyHash(string plainText,
                                       string hashAlgorithm,
                                       string hashValue)
