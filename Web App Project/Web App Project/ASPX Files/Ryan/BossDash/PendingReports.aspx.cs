a2d7b13ee3318834cda92b2e1faa2aa18f6efed4
Ryan <ryanchew96@gmail.com>
2016-07-13 09:46:26 +0800

    Updated Reports Approved

--- Web App Project/Web App Project/ASPX Files/Ryan/BossDash/PendingReports.aspx.cs
+++ Web App Project/Web App Project/ASPX Files/Ryan/BossDash/PendingReports.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -39,6 +41,81 @@ namespace Web_App_Project.Ryan
         protected void Button5_Click(object sender, EventArgs e)
         {
             Response.Redirect("Roster.aspx");
+        }
+        protected void GridView1_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            e.Row.Cells[4].Visible = false;
+        }
+
+        protected void OnRowDataBound(object sender, System.Web.UI.WebControls.GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                e.Row.Attributes["onclick"] = Page.ClientScript.GetPostBackClientHyperlink(GridView1, "Select$" + e.Row.RowIndex);
+                e.Row.Attributes["style"] = "cursor:pointer";
+            }
+        }
+
+        protected void OnSelectedIndexChanged(object sender, EventArgs e)
+        {
+            using (SqlConnection myConnection = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["localdbConnectionString1"].ConnectionString))
+            {
+                GridViewRow row = GridView1.SelectedRow;
+                TextBox1.Text = row.Cells[0].Text;
+                TextBox2.Text = row.Cells[1].Text;
+                TextBox3.Text = row.Cells[2].Text;
+                TextBox4.Text = row.Cells[3].Text;
+                TextBox5.Text = row.Cells[4].Text;
+
+                myConnection.Open();
+
+                string caseno = row.Cells[0].Text;
+                string Scmd = "SELECT Photo FROM Report WHERE CaseNo='" + caseno + "'";
+                SqlCommand cmd = new SqlCommand(Scmd, myConnection);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                reader.Read();
+
+                if (reader.HasRows)
+                {
+                    Label1.Text = reader[0].ToString();
+                    byte[] imagem = (byte[])(reader[1]);
+                    string base64String = Convert.ToBase64String(imagem);
+
+                    MemoryStream ms = new MemoryStream(imagem);
+
+                    Image1.ImageUrl = String.Format("data:image/jpg;base64,{0}", base64String);
+                }
+            }
+        }
+
+        protected void Button6_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection myConnection = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["localdbConnectionString1"].ConnectionString))
+            {
+                string caseno = TextBox1.Text;
+                string query = "UPDATE Report SET Status='approved' WHERE CaseNo='" + caseno + "'";
+
+                SqlCommand cmd = new SqlCommand(query, myConnection);
+                myConnection.Open();
+                cmd.ExecuteNonQuery();
+                myConnection.Close();
+            }
+        }
+
+        protected void Button7_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection myConnection = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["localdbConnectionString1"].ConnectionString))
+            {
+                string caseno = TextBox1.Text;
+                string query = "UPDATE Report SET Status='rejected' WHERE CaseNo='" + caseno + "'";
+
+                SqlCommand cmd = new SqlCommand(query, myConnection);
+                myConnection.Open();
+                cmd.ExecuteNonQuery();
+                myConnection.Close();
+            }
         }
     }
 }
