d45e0fd33ce14bad42d4e23fd94917fb9690237a
Joanne <limruoqijoanne54@gmail.com>
2016-07-20 22:41:46 +0800

    asdsf

--- Web App Project/Web App Project/ASPX Files/Joanne/VerifyEmailReset.aspx.cs
+++ Web App Project/Web App Project/ASPX Files/Joanne/VerifyEmailReset.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Web_App_Project.ASPX_Files.Joanne
 {
@@ -16,13 +18,30 @@ namespace Web_App_Project.ASPX_Files.Joanne
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-            //verify if input is equals to random generated number sent to person's email
+            //using (SqlConnection myConnection = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["localdbConnectionString1"].ConnectionString))
+            //{
+            //    //verify if input is equals to random generated number sent to person's email
 
-            // Generate a new 12-character password with 1 non-alphanumeric character.
+            String pwdInput = TextBox1.Text;
+            //    // Generate a new 12-character password with 1 non-alphanumeric character.
             string password = System.Web.Security.Membership.GeneratePassword(24, 0);
 
-            //print out the password and replace label3 to test :)
+            //    string query = "INSERT INTO Accounts (randomNumber)";
+            //    query += "VALUES (@randomNumber)" + "WHERE userid=;
+
+            //    SqlCommand myCommand = new SqlCommand(query, myConnection);
+
+            //    myCommand.Parameters.AddWithValue("@randomNumber", password);
+
+
+            //    myConnection.Open();
+            //    myCommand.ExecuteNonQuery();
+            //    myConnection.Close();
+
+
+            //    //print out the password and replace label3 to test :)
             Label3.Text = password;
+            //}
         }
 
         public static string GeneratePassword(int length, int numberOfNonAlphanumericCharacters)
