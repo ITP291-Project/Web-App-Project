a1432a983cf741edbae32b3b81dfa99b654b2459
Joanne <limruoqijoanne54@gmail.com>
2016-07-30 22:58:36 +0800

    fking done email

--- Web App Project/Web App Project/ASPX Files/Joanne/enterEmail.aspx.cs
+++ Web App Project/Web App Project/ASPX Files/Joanne/enterEmail.aspx.cs
@@ -7,7 +7,6 @@ using System.Web.UI.WebControls;
 using System.Data;
 using System.Data.SqlClient;
 using System.IO;
-using System.Net.Mail;
 using System.Web.UI.HtmlControls;
 
 using Google.Apis.Auth.OAuth2;
@@ -19,8 +18,8 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
+using System.Net;
 using System.Net.Mail;
-using System.Text;
 
 
 namespace Web_App_Project.ASPX_Files.Joanne
@@ -44,11 +43,13 @@ namespace Web_App_Project.ASPX_Files.Joanne
 
 
                 string query = "SELECT [Email] FROM [Accounts] WHERE [Email] = '" + inputEmail + "'";
-                string query1 = "UPDATE [Accounts] SET randomEmailString = @code WHERE Email = @email";
+                string query1 = "UPDATE [Accounts] SET [randomEmailString] = @code WHERE [Email] = @email";
 
                 SqlCommand myCommand = new SqlCommand(query, myConnection);
+                SqlCommand myCommand1 = new SqlCommand(query1, myConnection);
                 myConnection.Open();
                 myCommand.CommandType = CommandType.Text;
+                myCommand1.CommandType = CommandType.Text;
                 SqlDataReader reader = myCommand.ExecuteReader();
 
                 String dbEmail = "";
@@ -58,17 +59,10 @@ namespace Web_App_Project.ASPX_Files.Joanne
                     dbEmail = reader["Email"].ToString(); //read db email
                 }
                 myConnection.Close();
-                SqlCommand myCommand1 = new SqlCommand(query1, myConnection);
-                
-                //string query1 = "UPDATE [Accounts] SET [randomEmailString] = @randomEmailString WHERE [Email] = @email";
-
                 myConnection.Open();
-                //SqlDataReader reader1 = myCommand1.ExecuteReader();
 
                 myCommand1.Parameters.AddWithValue("@email", inputEmail);
                 myCommand1.Parameters.AddWithValue("@code", code);
-                myCommand1.CommandType = CommandType.Text;
-                System.Diagnostics.Debug.WriteLine(myCommand1);
                 myCommand1.ExecuteNonQuery();
 
                 //if email input not equals to db email - email does not exist
@@ -83,32 +77,23 @@ namespace Web_App_Project.ASPX_Files.Joanne
                     //Label2.Text = code;
                     Response.AppendHeader("Refresh", "5;url=VerifyEmailReset.aspx");
                     Label3.Text = "Email sent. Please check your email. You will now be redirected to verfiy code in 5 seconds";
+
+                    var smtp = new System.Net.Mail.SmtpClient();
+                    {
+                        smtp.Host = "smtp.gmail.com";
+                        smtp.Port = 587;
+                        smtp.EnableSsl = true;
+                        smtp.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
+                        smtp.Credentials = new NetworkCredential("limruoqijoanne54@gmail.com", "Basketball12345");
+                        smtp.Timeout = 20000;
+                    }
+
+                    smtp.Send("limruoqijoanne54@gmail.com", inputEmail, "Please Verify Your Email", "Please copy this code to verify your email." + code);
+                    //format - From, To, Subject, Body
+
+                    //smtp.Send("limruoqijoanne54@gmail.com", "152225K@mymail.nyp.edu.sg", "Testing", code);
+
                     myConnection.Close();
-
-                    //MailMessage mail = new MailMessage();
-                    //SmtpClient client = new SmtpClient();
-                    //client.Port = 587;
-                    //client.Host = "smtp.gmail.com";
-                    //client.EnableSsl = true;
-                    //client.Timeout = 10000;
-                    //client.DeliveryMethod = SmtpDeliveryMethod.Network;
-                    //client.UseDefaultCredentials = false;
-                    //client.Credentials = new System.Net.NetworkCredential("user@gmail.com", "password");
-                    //mail.From = new MailAddress("joannelimpotato@gmail.com");
-                    //mail.To.Add(new MailAddress(inputEmail));
-                    //mail.Subject = "testing";
-                    //mail.Body = "test";
-                    //client.Send(mail);
-
-                    //mail.BodyEncoding = UTF8Encoding.UTF8;
-                    //mail.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
-
-
-                    //Message email = new Message();
-                    //GmailService service = new GmailService();
-                    //Label2.Text = "Email exists";
-                    //SendMessage(service, "limruoqijoanne54@gmail.com", email);
-                    //Response.Redirect("emailSent.aspx");
                 }
 
             }
