30693f83a7c6e791d76ac9b77eef37ce6d231263
Ryan <ryanchew96@gmail.com>
2016-08-05 18:32:33 +0800

    Fixed joanne volunteer error

--- Web App Project/Web App Project/ASPX Files/Joanne/login.aspx.cs
+++ Web App Project/Web App Project/ASPX Files/Joanne/login.aspx.cs
@@ -281,7 +281,7 @@ namespace Web_App_Project.ASPX_Files.Joanne
                         //Is this line being executed?
                         System.Diagnostics.Debug.WriteLine("User is volunteer");
 
-                        Response.Redirect("/ASPX Files/Ryan/VolunteerDash/volunteerDash.aspx");
+                        Response.Redirect("/ASPX Files/Angie/VolunteerDash/volunteerDash.aspx");
                     }
 
                     else
